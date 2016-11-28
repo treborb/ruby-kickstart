61c908b128ae61fa95da27a695822012341ed34d
Rob Brentnall <rob.brentnall@gmail.com>
2016-11-28 19:59:18 +0000

    Chapter 1 - Exercise 7 Complete

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,15 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  new_string = ""
+  string.size.times.each do |i|
+    if string[i + 1] != nil && (string[i] == "r" || string[i] == "R")
+      new_string << string[i + 1]
+    end
+  end
+  return new_string
 end
