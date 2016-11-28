9c61cdfef4658ea5e09b7ebd82988b142dadeef6
Rob Brentnall <rob.brentnall@gmail.com>
2016-11-28 20:30:06 +0000

    Chapter 2 - Exercise 1: Complete

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -3,10 +3,16 @@
 
 # Write a program that reads in two integers typed on the keybaord
 # and outputs their sum, difference, and product
-# 
+#
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
+  print "Please type a number, followed by a space and then another number, then press enter:"
+  user_input = gets.chomp.split(" ")
+  first_int = user_input[0].to_i
+  second_int = user_input[1].to_i
+  puts first_int + second_int
+  puts first_int - second_int
+  puts first_int * second_int
 end
