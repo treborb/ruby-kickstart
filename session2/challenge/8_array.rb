bbe601492770ba01e557cfa056b812195c104a8e
Rob Brentnall <rob.brentnall@gmail.com>
2016-11-30 19:50:49 +0000

    Chapter 2 - Exercise 8: Complete

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,20 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+#def got_three?(array)
+#    array.each { |i| i == (array[i + 1] && array[i + 2]) ? true : false }
+#end
+def got_three?(array)
+    x = ""
+    y = ""
+    z = ""
+    array.each do |i|
+        z = i if (y == i && x == i)
+        return true if z != "" && (x == y && y == z)
+        y = "" && x = "" if (z != x && z != y)
+        y = i if x == i
+        x = i
+    end
+    return false
+end
