9564efbe27c65b8b5afee4bd72c71df18f518cab
Rob Brentnall <rob.brentnall@gmail.com>
2016-12-02 12:10:45 +0000

    Chapter 3 - Exercise 3: Complete

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,15 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(*args)
+    my_hash = {}
+    lowercase = args.select { |i| i.downcase? }
+    num_times = lowercase.map.each { |i| lowercase.count(i) }
+    1.upto args.size do |counter|
+        my_key = lowercase.each { |i| i }
+        my_val = num_times.each { |i| i }
+        my_hash[my_key] = my_val
+    end
+  return my_hash
+end
